4dbd5b8d551b2d64559e37d162e4bc25c405e852
Penquuin <64629679+Penquuin@users.noreply.github.com>
2021-12-31 17:58:02 +0800

    Alleviated the junction problem of gh-pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { HomeComponent } from "./dynamic/home/home.component";
 import { OopsComponent } from "./dynamic/oops/oops.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/home", pathMatch: "full" },
-  { path: "home", component: HomeComponent },
+  { path: "", component: HomeComponent },
   { path: "**", component: OopsComponent },
 ];
 
